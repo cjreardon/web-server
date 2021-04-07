f8612f0c0e6f68dd031638451daceeea937ffa67
cjreardon <connorreardon3@gmail.com>
2021-04-07 14:40:10 -0400

    checks if a files is on the server

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # web-server
-web server written in python
+web server written in Javascript.
+
+To run the server: "node webserver.js"
+
+To connect to the server once it's running: "telnet localhost 9999"
+    You should see the server log "Listening"
+
+Back on the terminal running telnet: "GET /test.txt HTTP/1.1" 
+    This returns a 200 ok because the file "test.txt" exists in the server's directory.
+    "GET /testing.txt HTTP/1.1"
+    This returns a 404 Not Found because "testing.txt" does not exist in the server's directory.
---
+++ webserver.js
@@ -0,0 +1,32 @@
+const net = require('net');
+const fs = require('fs');
+
+const server = net.createServer((socket) => {
+    socket.on("data", (buffer) => {
+        const requestString = buffer.toString('utf-8')
+
+        const request = parseRequest(requestString)
+
+        console.log(request.method, request.path, request.protocol);
+
+        if (request.method == "GET"){
+            if (fs.existsSync(`.${request.path}`)){
+                socket.write("HTTP/1.1 200 OK")
+            } else {
+                socket.write("HTTP/1.1 404 Not Found")
+            }
+        }
+    })
+})
+
+const parseRequest = (requestString) => {
+    const [method, path, protocol] = requestString.split(" ");
+
+    return {
+        method,
+        path,
+        protocol
+    }
+}
+
+server.listen(9999, () => console.log("Listening"))
